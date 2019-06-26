7df109d10798fb3bc0cdc703921ee95cf9d8aa0c
Colin Fahrion <colin.fahrion@gmail.com>
2019-06-25 21:32:48 -0700

    changed to Logictwine dev

--- src/util/getAPIurl.js
+++ src/util/getAPIurl.js
@@ -1,15 +1,15 @@
-const isProdution = true; //set to true to use bigbadcon.com
+const isProdution = false; //set to true to use bigbadcon.com
 
 const getAPIurl = (type) => {
-    const eventAPIurl = isProdution? "https://www.bigbadcon.com:8091/api/" : "http://www.logictwine.com:8092/"
+    const eventAPIurl = isProdution? "https://www.bigbadcon.com:8091/api/" : "https://www.logictwine.com:8091/api/"
 
     switch(type){
         case "posts":
             return 'https://www.bigbadcon.com/wp-json/wp/v2/posts'; //only set up to connect to BBC for blog posts at the moment
         case "events":
-            return eventAPIurl + '/events/all';
+            return eventAPIurl + 'events/all';
         case "login":
-            return eventAPIurl + '/login';
+            return eventAPIurl + 'login';
         default:
             return false;
     }
